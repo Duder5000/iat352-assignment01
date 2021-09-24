ef5458bd4dbc4802e2b970b7275e4800eeef9564
Alex <soames.alex@gmail.com>
2021-09-24 16:32:28 -0700

    visual change

--- footer.php
+++ footer.php
@@ -1,9 +1,14 @@
+        </div>
+      </div>
 
-<!--footer starts here-->
-</td>
-</tr>
-</table>
-</body>
+    </div>
+
+<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+    <div class="w3-col l4 img-card">
+          <a href="index.html"><img src="img/duder-logo.png" class="side-img"></a>
+    </div>
+
+  </body>
+
 </html>
-
-
