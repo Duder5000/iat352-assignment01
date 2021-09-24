ad5547db32d4da2c9bda418ee7ebc0695acb96c7
Duder <soames.alex@gmail.com>
2021-09-24 11:54:57 -0700

    Starter Files

---
+++ html-top.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Alex Soames</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/w3.css">
+    <link rel="stylesheet" href="css/dakrmode.css">
+    <link rel="icon" href="img/duder-logo.png">
+  </head>
+
+  <body>
+    <div class="w3-content" style="max-width:1400px">
+
+    <header class="w3-container w3-center"> 
+      <h1><a href="index.html" class="home-link">Alex Soames | IAT 352</a></h1>
+    </header>
+
+    <div class="w3-row">
+
+    <div class="w3-col l8 s12">
+      
+      <div class="w3-card-4 w3-margin card">
+        <div id="a01" class="w3-container">
+          <h2>
+            <b>Assignment 01</b>
+          </h2>        
